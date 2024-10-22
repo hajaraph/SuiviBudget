a84e494ffde9a41e204b0f15a6b8f4d36205abca
Demondra <rahajarisonraphael@gmail.com>
2024-10-22 10:37:28 -0700

    CRUD service fini

--- Consommation/serializers.py
+++ Consommation/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
+from Consommation.models import Service
+
 Utilisateur = get_user_model()
 
 
@@ -14,25 +16,42 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('nom_utilisateur', 'password', 'password_confirm', 'est_admin', 'numero_utilisateur', 'service')
 
     def validate(self, attrs):
-        # Vérification que les deux mots de passe correspondent
         if attrs['password'] != attrs['password_confirm']:
             raise serializers.ValidationError({'password': 'Les mots de passe ne correspondent pas.'})
 
         return attrs
 
     def create(self, validated_data):
-        # Supprimer le champ password_confirm avant la création de l'utilisateur
         validated_data.pop('password_confirm')
 
         # Créer l'utilisateur
         utilisateur = Utilisateur.objects.create(
             nom_utilisateur=validated_data['nom_utilisateur'],
             numero_utilisateur=validated_data['numero_utilisateur'],
-            service=validated_data['service'],  # Utilisez 'service' et non 'service_id'
-            est_admin=validated_data.get('est_admin', False)  # Utilisez la valeur par défaut ici
+            service=validated_data['service'],
+            est_admin=validated_data.get('est_admin', False)
         )
 
         # Hacher le mot de passe
         utilisateur.set_password(validated_data['password'])
         utilisateur.save()
         return utilisateur
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    nom_service = serializers.CharField(required=True)
+
+    class Meta:
+        model = Service
+        fields = ('nom_service',)
+
+    def create(self, validated_data):
+        return Service.objects.create(
+            nom_service=validated_data['nom_service'],
+        )
+
+    def update(self, instance, validated_data):
+        instance.nom_service = validated_data.get('nom_service', instance.nom_service)
+        instance.save()
+        return instance
+
